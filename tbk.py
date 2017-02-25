d095fccddcfef36b7ebc09feeeeea64c872d1331
zzqandy <zzqandy@163.com>
2017-02-25 20:35:29 +0800

    update

--- tbk.py
+++ tbk.py
@@ -3,7 +3,7 @@ import requests
 import json
 import sqlite3
 import datetime
-now=datetime.datetime.now()
+now = datetime.datetime.now()
 today = now.strftime('%Y-%m-%d')
 
 headers = {
@@ -12,22 +12,24 @@ headers = {
     }
 with sqlite3.connect('tbk.db') as con:
     cur = con.cursor()
-    '''cur.execute('create table tbk(id integer primary key autoincrement,title varchar,\
-    shopTitle text,auctionId varchar,zkPrice FLOAT,eventRate FLOAT,tkCommFee FLOAT,dayLeft int,couponAmount int,endTime varchar,\
-    couponInfo varchar,auctionUrl text,pictUrl text)')'''
+
+    def creat_tab():
+        cur.execute('create table tbk(id integer primary key autoincrement,title varchar,\
+        shopTitle text,auctionId varchar,zkPrice FLOAT,eventRate FLOAT,tkCommFee FLOAT,dayLeft int,couponAmount int,\
+        endTime varchar,couponInfo varchar,auctionUrl text,pictUrl text)')
     
     '''条目ID，商品标题，店铺名称，商品ID，折扣价格，佣金比例，利润金额，剩余天数，优惠券面额，优惠券有效期，优惠券信息'''
+
     def catch_json():
-        for i in range(1,101):
+        for p in range(1, 101):
             try:
-                print(i)
+                print(p)
                 r = requests.get('''http://pub.alimama.com/items/channel/qqhd.json?channel=qqhd&toPage={}&\
-dpyhq=1&perPageSize=100&shopTag=dpyhq'''.format(i), headers=headers).text
+dpyhq=1&perPageSize=100&shopTag=dpyhq'''.format(p), headers=headers).text
                 dj = json.loads(r)['data']['pageList']
                 for i in dj:
                     title = str(i['title']) #商品标题
                     shopTitle = str(i['shopTitle']) #店铺名称
-                    #print(shopTitle)
                     auctionId = str(i['auctionId']) #商品ID
                     zkPrice = float(i['zkPrice']) #折扣价格
                     eventRate = float(i['eventRate']) #佣金比例
@@ -40,15 +42,18 @@ dpyhq=1&perPageSize=100&shopTag=dpyhq'''.format(i), headers=headers).text
                     pictUrl = str(i['pictUrl']) #主图链接
                     cur.execute("insert into tbk(id,title,shopTitle,auctionId,zkPrice,eventRate,tkCommFee,dayLeft,couponAmount,endTime,\
                     couponInfo,auctionUrl,pictUrl)values(null,'{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(title.replace("'", ''), \
-                    shopTitle.replace("'", '-'),auctionId,zkPrice,eventRate,tkCommFee,dayLeft,couponAmount,endTime,couponinfo,auctionUrl,pictUrl))
+                    shopTitle.replace("'", '-'), auctionId, zkPrice, eventRate, tkCommFee, dayLeft, couponAmount, endTime, couponinfo, auctionUrl, pictUrl))
                 con.commit()
             except requests.exceptions as e:
-                print('requests.exceptions')
+                print('requests.exceptions', e)
 
     def l():
-        cur.execute('select id,title,shopTitle,zkPrice,eventRate,tkCommFee,couponinfo from tbk where couponAmount > 30 and zkPrice < 60 and eventRate>50')
+        cur.execute('select id,title,shopTitle,zkPrice,eventRate,tkCommFee,couponinfo from tbk where \
+        couponAmount > 30 and zkPrice < 60 and eventRate>50')
         d = cur.fetchall()
         for i in d:
             print(i)
+
+    #creat_tab()
     #catch_json()
     l()
